28e60393ec86c4fa47483dbb8461314afbd91242
Arek Kesiz Abnousi <99053976+Arek-KesizAbnousi@users.noreply.github.com>
2024-12-03 18:58:19 -0500

    Create gan_training.py

---
+++ src/gan_training.py
@@ -0,0 +1,16 @@
+# gan_training.py
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
+from neural_networks import Generator, Discriminator
+from data_preprocessing import get_datasets
+
+def train_gan():
+    # [Load datasets]
+    # [Initialize models, loss functions, optimizers]
+    # [Training loop]
+    # [Save generated images]
+
+if __name__ == '__main__':
+    train_gan()
